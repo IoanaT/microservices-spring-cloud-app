78b3163248379efb684667e1136e8aa74f4799e4
Ioana Tiriac <ioana.tiriac@gmail.com>
2021-01-23 03:41:12 +0100

    find compatible spring.cloud version for spring boot 2.4.x

--- src/main/java/com/microservices.spring.cloud/app/ws/UserWsApplication.java
+++ src/main/java/com/microservices.spring.cloud/app/ws/UserWsApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class MicroservicesApplication {
+public class UserWsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroservicesApplication.class, args);
+		SpringApplication.run(UserWsApplication.class, args);
 	}
 }
