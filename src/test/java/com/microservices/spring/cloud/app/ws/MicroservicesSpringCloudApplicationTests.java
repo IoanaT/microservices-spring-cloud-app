ab8b84f0242b605236b56e8195fad4266610f4fd
Ioana Tiriac <ioana.tiriac@gmail.com>
2021-01-23 01:03:59 +0100

    add submodule discovery-service to project

--- src/test/java/com/microservices/spring/cloud/app/ws/MicroservicesSpringCloudApplicationTests.java
+++ src/test/java/com/microservices/spring/cloud/app/ws/MicroservicesSpringCloudApplicationTests.java
@@ -1,4 +1,4 @@
-package com.appsdeveloperblog.app.ws;
+package com.microservices.spring.cloud.app.ws;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MobileAppWsApplicationTests {
+public class MicroservicesSpringCloudApplicationTests {
 
 	@Test
 	public void contextLoads() {
