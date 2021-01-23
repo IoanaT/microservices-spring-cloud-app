3b2a4a66184b5d0c529e77c0f65e5efab27d0030
Ioana Tiriac <ioana.tiriac@gmail.com>
2021-01-23 02:24:39 +0100

    make user service an eureka client

--- src/main/java/com/microservices.spring.cloud/app/ws/MicroservicesApplication.java
+++ src/main/java/com/microservices.spring.cloud/app/ws/MicroservicesApplication.java
@@ -2,8 +2,10 @@ package com.microservices.spring.cloud.app.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroservicesApplication {
 
 	public static void main(String[] args) {
