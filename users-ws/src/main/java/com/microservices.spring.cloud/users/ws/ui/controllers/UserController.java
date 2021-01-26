0f36b6ff8444713992e552ccfd18be01c596e41a
Ioana Tiriac <ioana.tiriac@gmail.com>
2021-01-26 17:59:27 +0100

    add git submodule api-gateway and add status check endpoint to controller

--- users-ws/src/main/java/com/microservices.spring.cloud/users/ws/ui/controllers/UserController.java
+++ users-ws/src/main/java/com/microservices.spring.cloud/users/ws/ui/controllers/UserController.java
@@ -1,4 +1,4 @@
-package com.microservices.spring.cloud.app.ws.ui.controllers;;
+package com.microservices.spring.cloud.users.ws.ui.controllers;
 
 import java.util.Map;
 
@@ -27,74 +27,73 @@ import com.appsdeveloperblog.app.ws.ui.model.response.UserRest;
 @RequestMapping("/users")
 public class UserController {
 
-	Map<String, UserRest> users;
-	
-	@Autowired
-	UserService userService;
-	
-	@GetMapping
-	public String getUsers(@RequestParam(value="page", defaultValue="1") int page, 
-			@RequestParam(value="limit", defaultValue="50") int limit,
-			@RequestParam(value="sort", defaultValue = "desc", required = false) String sort)
-	{
-		return "get users was called with page = " + page + " and limit = " + limit + " and sort = " + sort;
-	}
-	
-	@GetMapping(path="/{userId}", 
-			produces =  { 
-					MediaType.APPLICATION_XML_VALUE,
-					MediaType.APPLICATION_JSON_VALUE
-					} )
-	public ResponseEntity<UserRest> getUser(@PathVariable String userId)
-	{
-		if(users.containsKey(userId))
-		{
-			return new ResponseEntity<>(users.get(userId), HttpStatus.OK);
-		} else {
-			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-		}
-	}
-	
-	@PostMapping(
-			consumes =  { 
-			MediaType.APPLICATION_XML_VALUE,
-			MediaType.APPLICATION_JSON_VALUE
-			}, 
-			produces =  { 
-					MediaType.APPLICATION_XML_VALUE,
-					MediaType.APPLICATION_JSON_VALUE
-					}  )
-	public ResponseEntity<UserRest> createUser(@Valid @RequestBody UserDetailsRequestModel userDetails)
-	{
+    Map<String, UserRest> users;
 
-		UserRest returnValue = userService.createUser(userDetails);
-		return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
-	}
-	
-	@PutMapping(path="/{userId}", consumes =  { 
-			MediaType.APPLICATION_XML_VALUE,
-			MediaType.APPLICATION_JSON_VALUE
-			}, 
-			produces =  { 
-					MediaType.APPLICATION_XML_VALUE,
-					MediaType.APPLICATION_JSON_VALUE
-					}  )
-	public UserRest updateUser(@PathVariable String userId, @Valid @RequestBody UpdateUserDetailsRequestModel userDetails)
-	{
-		 UserRest storedUserDetails = users.get(userId);
-		 storedUserDetails.setFirstName(userDetails.getFirstName());
-		 storedUserDetails.setLastName(userDetails.getLastName());
-		 
-		 users.put(userId, storedUserDetails);
-		 
-		 return storedUserDetails;
-	}
-	
-	@DeleteMapping(path="/{id}")
-	public ResponseEntity<Void> deleteUser(@PathVariable String id)
-	{
-		users.remove(id);
-		
-		return ResponseEntity.noContent().build();
-	}
+    @Autowired
+    UserService userService;
+
+    @GetMapping
+    public String getUsers(@RequestParam(value = "page", defaultValue = "1") int page,
+                           @RequestParam(value = "limit", defaultValue = "50") int limit,
+                           @RequestParam(value = "sort", defaultValue = "desc", required = false) String sort) {
+        return "get users was called with page = " + page + " and limit = " + limit + " and sort = " + sort;
+    }
+
+    @GetMapping(path = "/{userId}",
+            produces = {
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_JSON_VALUE
+            })
+    public ResponseEntity<UserRest> getUser(@PathVariable String userId) {
+        if (users.containsKey(userId)) {
+            return new ResponseEntity<>(users.get(userId), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+    }
+
+    @PostMapping(
+            consumes = {
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_JSON_VALUE
+            },
+            produces = {
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_JSON_VALUE
+            })
+    public ResponseEntity<UserRest> createUser(@Valid @RequestBody UserDetailsRequestModel userDetails) {
+
+        UserRest returnValue = userService.createUser(userDetails);
+        return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
+    }
+
+    @PutMapping(path = "/{userId}", consumes = {
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_JSON_VALUE
+    },
+            produces = {
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_JSON_VALUE
+            })
+    public UserRest updateUser(@PathVariable String userId, @Valid @RequestBody UpdateUserDetailsRequestModel userDetails) {
+        UserRest storedUserDetails = users.get(userId);
+        storedUserDetails.setFirstName(userDetails.getFirstName());
+        storedUserDetails.setLastName(userDetails.getLastName());
+
+        users.put(userId, storedUserDetails);
+
+        return storedUserDetails;
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable String id) {
+        users.remove(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(path = "/status/check")
+    public String getStatus() {
+		return "Working...";
+    }
 }
