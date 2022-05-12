0c6f1b66c0763f15041b8e496701c83de2db5c30
Ioana Tiriac <ioana.tiriac@gmail.com>
2022-05-12 23:40:56 +0200

    add new albums classes

---
+++ account-ws/src/main/java/microservices/spring/cloud/albums/ws/AlbumsApplication.java
@@ -0,0 +1,16 @@
+package microservices.spring.cloud.albums.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AlbumsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumsApplication.class, args);
+	}
+
+}
+
