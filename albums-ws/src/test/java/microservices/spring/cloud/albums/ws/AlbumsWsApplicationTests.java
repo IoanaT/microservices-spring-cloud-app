6a36e78258925d80c8d54d360872afd2d80d52a4
Ioana Tiriac <ioana.tiriac@gmail.com>
2022-05-16 13:21:24 +0200

    refactoring albums microservice with appropiate directory names

---
+++ albums-ws/src/test/java/microservices/spring/cloud/albums/ws/AlbumsWsApplicationTests.java
@@ -0,0 +1,14 @@
+package microservices.spring.cloud.albums.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AlbumsWsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
