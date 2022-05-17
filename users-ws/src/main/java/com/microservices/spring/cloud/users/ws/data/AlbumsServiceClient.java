5da5046b1075560b8969138f3f804fb1beec040c
Ioana Tiriac <ioana.tiriac@gmail.com>
2022-05-17 19:53:39 +0200

    add resilience4j circuit breaker and fallback method to users microservice

--- users-ws/src/main/java/com/microservices/spring/cloud/users/ws/data/AlbumsServiceClient.java
+++ users-ws/src/main/java/com/microservices/spring/cloud/users/ws/data/AlbumsServiceClient.java
@@ -1,14 +1,24 @@
 package com.microservices.spring.cloud.users.ws.data;
 
 import com.microservices.spring.cloud.users.ws.ui.model.response.AlbumResponseModel;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @FeignClient(name="albums-ws")
 public interface AlbumsServiceClient {
-    @GetMapping("/users/{id}/albumss")
+
+    @GetMapping("/users/{id}/albums")
+    @CircuitBreaker(name="albums-ws", fallbackMethod="getAlbumsFallback")
     List<AlbumResponseModel> getAlbums(@PathVariable String id);
+
+    default List<AlbumResponseModel> getAlbumsFallback(@PathVariable String id, Throwable exception){
+        System.out.println("User ID : " + id);
+        System.out.println("Exception took place: "+ exception.getMessage());
+        return new ArrayList<>();
+    }
 }
